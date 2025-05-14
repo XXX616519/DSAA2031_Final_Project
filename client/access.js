6f605c86c9b57e1013331ba79976512f43f11ae6
Altair Zhenzhuo Li <altairpaca@gmail.com>
2025-05-14 19:46:32 +0800

    add console error to declare hours in server

--- client/access.js
+++ client/access.js
@@ -615,7 +615,7 @@ else if (role == 0) {
   // ...existing code...
 
   // 学生使用的function:
-  
+
 
   // 取消申报工作时长的函数
   function cancelWorkingHours(projectId) {
@@ -638,13 +638,13 @@ else if (role == 0) {
   // 上传工作时间的函数，该函数会在点击“Upload”按钮时被调用
   function declareWorkingHours(projectId) {
     const workingHours = document.getElementById(`declareHours-${projectId}`).value;
-    const date= new Date().toLocaleDateString();
+    const date = new Date().toISOString().slice(0, 10); // 获取当前日期，格式为 YYYY-MM-DD
 
     // 通过api/upload-working-hours上传工作时间数据
     fetch('http://localhost:3000/api/declare-working-hours', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ pid:projectId,sid: userId, hours: Number(workingHours), date }) // 传入所有参数
+      body: JSON.stringify({ pid: projectId, sid: userId, hours: Number(workingHours), date }) // 传入所有参数
     })
       .then(response => response.json())
       .then(data => {
